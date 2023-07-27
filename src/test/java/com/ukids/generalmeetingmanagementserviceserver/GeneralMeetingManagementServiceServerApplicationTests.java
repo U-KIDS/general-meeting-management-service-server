048e897b9d0fd2dd7892a6ef5436c9384e35b1be
wwan13 <wwan13@naver.com>
2023-07-27 18:05:17 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/ukids/generalmeetingmanagementserviceserver/GeneralMeetingManagementServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukids.generalmeetingmanagementserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralMeetingManagementServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
