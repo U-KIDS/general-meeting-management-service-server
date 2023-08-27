ced178fe351ff43bd6c672321daad7983ba15c36
YYRi9 <mesmgr01@gmail.com>
2023-08-28 05:00:20 +0900

    modify : 사용하지 않는 메서드 삭제

--- src/main/java/io/ukids/generalmeetingmanagementsystem/domain/agenda/AgendaRepository.java
+++ src/main/java/io/ukids/generalmeetingmanagementsystem/domain/agenda/AgendaRepository.java
@@ -10,8 +10,6 @@ import java.util.Optional;
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
     @Override
     Optional<Agenda> findById(Long aLong);
-    Optional<List<Agenda>> findAllByMeeting_Name(String meetingName);
-    Optional<List<Agenda>> findAllByMeetingId(Long id);
     List<Agenda> findAllByMeeting_Id(Long id);
 }
 
