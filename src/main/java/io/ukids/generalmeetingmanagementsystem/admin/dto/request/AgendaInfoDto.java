f5d9d0a2bc5bb4499a14817c7816c8a9382d637a
wwan13 <wwan13@naver.com>
2023-08-31 16:13:25 +0900

    feat : request 로 들어오는 DTO들 기본 생성자 추가

--- src/main/java/io/ukids/generalmeetingmanagementsystem/admin/dto/request/AgendaInfoDto.java
+++ src/main/java/io/ukids/generalmeetingmanagementsystem/admin/dto/request/AgendaInfoDto.java
@@ -1,8 +1,11 @@
 package io.ukids.generalmeetingmanagementsystem.admin.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AgendaInfoDto {
     private String title;
 }
