149914b2d9a871f42068e239d9d15a931fda94ab
wwan13 <wwan13@naver.com>
2023-09-09 15:19:51 +0900

    fix : 유저 투표 관련 API 수정

--- src/main/java/io/ukids/generalmeetingmanagementsystem/client/controller/VoteClientController.java
+++ src/main/java/io/ukids/generalmeetingmanagementsystem/client/controller/VoteClientController.java
@@ -6,11 +6,11 @@ import io.ukids.generalmeetingmanagementsystem.common.dto.CreateDto;
 import io.ukids.generalmeetingmanagementsystem.common.response.ApiDataResponse;
 import io.ukids.generalmeetingmanagementsystem.common.response.ApiResponse;
 import io.ukids.generalmeetingmanagementsystem.common.response.HttpStatusCode;
+import io.ukids.generalmeetingmanagementsystem.common.util.SecurityUtil;
+import io.ukids.generalmeetingmanagementsystem.domain.agenda.enums.AgendaResult;
+import io.ukids.generalmeetingmanagementsystem.domain.vote.enums.VoteValue;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +23,15 @@ public class VoteClientController {
     public ApiResponse onClickVote(@RequestBody VoteClientRequestDto voteClientRequestDto) {
         CreateDto voteCreateDto = voteClientService.onClickVote(voteClientRequestDto);
         return ApiDataResponse.of(HttpStatusCode.OK, voteCreateDto, "투표를 완료했습니다.");
+    }
+
+    @PostMapping(value = "/agenda/{agendaId}/vote")
+    public ApiResponse vote(
+            @PathVariable Long agendaId,
+            @RequestParam VoteValue voteValue) {
+        String studentNumber = SecurityUtil.getCurrentStudentNumber();
+        voteClientService.vote(agendaId, studentNumber, voteValue);
+
+        return ApiResponse.of(HttpStatusCode.OK, "투표를 완료하였습니다.");
     }
 }
