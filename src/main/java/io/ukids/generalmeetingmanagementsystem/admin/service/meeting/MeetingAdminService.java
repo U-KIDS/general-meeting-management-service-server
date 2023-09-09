476b6553df3cbe9aa1500443a4f06091eefe0347
wwan13 <wwan13@naver.com>
2023-09-09 15:22:54 +0900

    fix : 회의 관련 API 수정

--- src/main/java/io/ukids/generalmeetingmanagementsystem/admin/service/meeting/MeetingAdminService.java
+++ src/main/java/io/ukids/generalmeetingmanagementsystem/admin/service/meeting/MeetingAdminService.java
@@ -1,25 +1,19 @@
 package io.ukids.generalmeetingmanagementsystem.admin.service.meeting;
 
-import io.ukids.generalmeetingmanagementsystem.admin.dto.response.MeetingDetailDto;
 import io.ukids.generalmeetingmanagementsystem.admin.dto.response.MeetingInfoDto;
-import io.ukids.generalmeetingmanagementsystem.admin.dto.response.MeetingListDto;
-import io.ukids.generalmeetingmanagementsystem.admin.service.agenda.AgendaAdminService;
 import io.ukids.generalmeetingmanagementsystem.common.dto.CreateDto;
-import io.ukids.generalmeetingmanagementsystem.common.dto.ListDto;
 import io.ukids.generalmeetingmanagementsystem.common.exception.BaseException;
 import io.ukids.generalmeetingmanagementsystem.common.exception.ErrorCode;
-import io.ukids.generalmeetingmanagementsystem.common.mapper.AgendaMapper;
 import io.ukids.generalmeetingmanagementsystem.common.mapper.MeetingMapper;
 import io.ukids.generalmeetingmanagementsystem.domain.agenda.Agenda;
 import io.ukids.generalmeetingmanagementsystem.domain.agenda.AgendaRepository;
+import io.ukids.generalmeetingmanagementsystem.domain.agendaimage.AgendaImageRepository;
 import io.ukids.generalmeetingmanagementsystem.domain.meeting.Meeting;
 import io.ukids.generalmeetingmanagementsystem.domain.meeting.MeetingRepository;
+import io.ukids.generalmeetingmanagementsystem.domain.vote.VoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -28,7 +22,8 @@ public class MeetingAdminService {
 
     private final MeetingRepository meetingRepository;
     private final AgendaRepository agendaRepository;
-    private final AgendaAdminService agendaAdminService;
+    private final AgendaImageRepository agendaImageRepository;
+    private final VoteRepository voteRepository;
     private final MeetingMapper meetingMapper;
 
     public CreateDto create(MeetingInfoDto meetingInfoDto) {
@@ -38,16 +33,16 @@ public class MeetingAdminService {
         return new CreateDto(id);
     }
 
-    public void start(Long id) {
+    public void activate(Long id) {
         Meeting meeting = meetingRepository.findById(id)
                 .orElseThrow(() -> new BaseException(ErrorCode.MEETING_NOT_FOUND));
-        meeting.start();
+        meeting.activate();
     }
 
-    public void end(Long id) {
+    public void deactivate(Long id) {
         Meeting meeting = meetingRepository.findById(id)
                 .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND));
-        meeting.end();
+        meeting.deactivate();
     }
 
 
@@ -58,8 +53,17 @@ public class MeetingAdminService {
     }
 
     public void delete(Long meetingId) {
+        System.out.println("asdasdasd");
         agendaRepository.findAllByMeetingId(meetingId)
-                        .forEach(agenda -> agendaAdminService.delete(agenda.getId()));
+                        .forEach(agenda -> deleteAgenda(agenda));
         meetingRepository.deleteById(meetingId);
     }
+
+    private void deleteAgenda(Agenda agenda) {
+        agendaImageRepository.deleteAllByAgenda(agenda);
+        voteRepository.deleteAllByAgenda(agenda);
+        agendaRepository.delete(agenda);
+    }
+
+
 }
